263232a5b517a2022d69483e35f9d4fb8e70c63a
adishiritwick <91323393+adishiritwick@users.noreply.github.com>
2021-10-20 17:00:03 +0530

    Update demo.ps1

--- demo.ps1
+++ demo.ps1
@@ -5,3 +5,5 @@ $Headers.Add("X-IDENTITY-HEADER", $env:IDENTITY_HEADER)
 $Headers.Add("Metadata", "True") 
 $accessToken = Invoke-RestMethod -Uri $url -Method 'GET' -Headers $Headers
 Write-Output $accessToken.access_token
+Write-Output $accessToken.access_token
+
